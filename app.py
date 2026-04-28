9fc48dbcec90c773eb50c08625c17645633f6006
Dammrow <dammrow112@gmail.com>
2026-04-28 16:14:31 +0530

    updated favicon

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import request
 from flask import Flask, render_template
 import sqlite3
 import smtplib
-import os
+# import os
 
 app = Flask(__name__)
 
@@ -77,5 +77,8 @@ def register():
 
     return 'Registration successfull'
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
